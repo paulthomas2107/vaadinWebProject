8a82eb0100f053185a0959f5b219ff754746af80
Paul Thomas <paulthomas2107@gmail.com>
2022-11-23 14:10:04 +0000

    More...

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,42 @@
 package com.example.demo;
 
+import com.example.demo.ui.LoginView;
+import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SpringBootApplication
-public class 	DemoApplication {
+public class DemoApplication extends VaadinWebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		super.configure(http);
+		setLoginView(http, LoginView.class);
+	}
+
+	@Override
+	@Bean
+	public UserDetailsService userDetailsServiceBean() throws Exception {
+		return new InMemoryUserDetailsManager(
+				User.withUsername("Paul210766@gmail.com")
+						.password("{noop}abcdefg01010101010101")
+						.roles("ADMIN")
+						.build(),
+				User.withUsername("Rory")
+						.password("{noop}rory")
+						.roles("ADMIN")
+						.build()
+		);
+	}
 }
+// Paul210766@gmail.com
+ // abcdefg01010101010101
---
+++ src/main/java/com/example/demo/ui/AdminView.java
@@ -0,0 +1,17 @@
+package com.example.demo.ui;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+import javax.annotation.security.RolesAllowed;
+
+@Route("admin")
+@RolesAllowed("ADMIN")
+public class AdminView extends VerticalLayout {
+    public AdminView() {
+        add(
+                new H1("Admin View")
+         );
+    }
+}

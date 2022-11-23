368d7d42c1839f40fd4a1ee3a275e169824854f9
Paul Thomas <paulthomas2107@gmail.com>
2022-11-23 14:27:50 +0000

    More...

--- src/main/java/com/example/demo/ui/AdminView.java
+++ src/main/java/com/example/demo/ui/AdminView.java
@@ -1,17 +1,25 @@
 package com.example.demo.ui;
 
+import com.example.demo.backend.Book;
+import com.example.demo.backend.BookService;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import org.vaadin.crudui.crud.impl.GridCrud;
 
 import javax.annotation.security.RolesAllowed;
 
 @Route("admin")
 @RolesAllowed("ADMIN")
 public class AdminView extends VerticalLayout {
-    public AdminView() {
+    public AdminView(BookService service) {
+        var crud = new GridCrud<>(Book.class, service);
+        crud.getGrid().setColumns("title", "publishedDate", "rating");
+        crud.getCrudFormFactory().setVisibleProperties("title", "publishedDate", "rating");
+
         add(
-                new H1("Admin View")
+                new H1("Admin View"),
+                crud
          );
     }
 }

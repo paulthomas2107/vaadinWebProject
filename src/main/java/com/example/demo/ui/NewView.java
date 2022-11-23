40303c47063416999999c004ffb20f17be972881
Paul Thomas <paulthomas2107@gmail.com>
2022-11-23 15:57:06 +0000

    More...

--- src/main/java/com/example/demo/ui/NewView.java
+++ src/main/java/com/example/demo/ui/NewView.java
@@ -3,17 +3,18 @@ package com.example.demo.ui;
 import com.example.demo.backend.Book;
 import com.example.demo.backend.BookService;
 import com.vaadin.collaborationengine.CollaborationBinder;
+import com.vaadin.collaborationengine.CollaborationMessageInput;
+import com.vaadin.collaborationengine.CollaborationMessageList;
 import com.vaadin.collaborationengine.UserInfo;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.Route;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -37,18 +38,29 @@ public class NewView extends VerticalLayout {
         binder.bindInstanceFields(this);
         binder.setTopic("new-book", () -> new Book());
 
-
+        var messageList = new CollaborationMessageList(userInfo, "new-book");
 
         add(
                 new H1("New Book"),
-                new FormLayout(title, publishedDate, rating),
-                new Button("Save", event -> {
-                    var book = new Book();
-                    binder.writeBeanIfValid(book);
-                    service.add(book);
-                    Notification.show("Book Saved !");
-                    binder.reset(new Book());
-                })
+
+                new HorizontalLayout(
+                        new VerticalLayout(new FormLayout(title, publishedDate, rating),
+                                new Button("Save", event -> {
+                                    var book = new Book();
+                                    binder.writeBeanIfValid(book);
+                                    service.add(book);
+                                    Notification.show("Book Saved !");
+                                    binder.reset(new Book());
+                                })
+                        ),
+                        new VerticalLayout(
+                                messageList,
+                                new CollaborationMessageInput(messageList)
+                        )
+                )
+
+
+
         );
     }
 
